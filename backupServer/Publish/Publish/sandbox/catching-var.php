57635232bc26742d8747dfa1e6cbfe94555880bf
levi Balling <leviballing@gmail.com>
2012-12-04 13:30:52 -0700

    added the webpages to the server

---
+++ backupServer/Publish/Publish/sandbox/catching-var.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?php $name0 = $_POST['name0'];
+
+$name1 = $_POST['name1']; 
+$name2 = $_POST['name2']; 
+$name3 = $_POST['name3']; 
+$name4 = $_POST['name4'];
+
+echo $name0.'<br/><br/>'; 
+echo $name1.'<br/><br/>'; 
+echo $name2.'<br/><br/>'; 
+echo $name3.'<br/><br/>'; 
+echo $name3.'<br/><br/>';
+
+?>
+
+
+</body>
+</html>
